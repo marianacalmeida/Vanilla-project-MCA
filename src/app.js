14da2f30adb730c54ffaf448a06bc625300926f2
marianacalmeida <marianacalmeida@users.noreply.github.com>
2021-01-28 17:36:25 +0000

    Added JS date

--- src/app.js
+++ src/app.js
@@ -1,18 +1,42 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+return `${day}, ${hours}:${minutes}`
+}
+
 
 function displayTemperature(response) {
     let mainTemp = Math.round(response.data.main.temp);
     let tempElement = document.querySelector("#currentTemp");
     tempElement.innerHTML = `${mainTemp}º`;
+//
     let city = response.data.name;
      let cityElement = document.querySelector("#city");
     cityElement.innerHTML = city;
+    //
     let minTemp = Math.round(response.data.main.temp_min)
     let maxTemp = Math.round(response.data.main.temp_max)
      let minElement = document.querySelector("#min");
     minElement.innerHTML = `${minTemp}º`;
     let maxElement = document.querySelector("#max");
     maxElement.innerHTML = `${maxTemp}º`;
-    
+    //
+     let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000)
+    //
+
 }
 
 let apiKey = "faeef537be5414427a6c70a51cd4c87e";
