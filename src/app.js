074eb1cdb54d82587f6ae061240f5dc2fe5524c7
marianacalmeida <marianacalmeida@users.noreply.github.com>
2021-01-29 09:09:26 +0000

    Added API icon

--- src/app.js
+++ src/app.js
@@ -12,7 +12,14 @@ let days = [
 ];
     let day = days[date.getDay()];
     let hours = date.getHours();
+    if (hours < 10){
+        hours = `0${hours}`
+    };
     let minutes = date.getMinutes();
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    };
+    
 return `${day}, ${hours}:${minutes}`
 }
 
@@ -34,13 +41,15 @@ function displayTemperature(response) {
     maxElement.innerHTML = `${maxTemp}º`;
     //
      let dateElement = document.querySelector("#date");
-    dateElement.innerHTML = formatDate(response.data.dt * 1000)
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     //
-
+    let iconElement = document.querySelector("#icon");
+    let iconCode = response.data.weather[0].icon;
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${iconCode}@2x.png`);
 }
 
 let apiKey = "faeef537be5414427a6c70a51cd4c87e";
-
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Porto&appid=${apiKey}&units=metric`;
+let city = "Porto"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
